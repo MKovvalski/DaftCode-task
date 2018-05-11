62abf5930db47ba7011de2017839510dbc20e7fd
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-05-11 19:10:09 +0200

    ready RWD mobile version

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import Sass from "./main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  // rotation effect on kolko_kwadrat element
+
   const circleSquare = document.querySelector(".circle-square-image");
 
   circleSquare.addEventListener("click", function () {
@@ -13,5 +15,20 @@ document.addEventListener("DOMContentLoaded", () => {
     void circleSquare.offsetWidth;
     circleSquare.classList.add("rotate");
   });
+
+  // sliding-down/sliding-up mobile nav menu
+
+  const mobileNav = document.querySelector(".main-nav");
+  const burgerButton = document.querySelector(".burger-button");
+
+  burgerButton.addEventListener("click", function () {
+    if (mobileNav.classList.contains("slide-down")) {
+      mobileNav.classList.remove("slide-down");
+      mobileNav.classList.add("slide-up");
+    } else {
+      mobileNav.classList.remove("slide-up");
+      mobileNav.classList.add("slide-down");
+    }
+  })
 
 });
