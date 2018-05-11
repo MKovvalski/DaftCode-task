3417f1f32c8c61c35105763705673022d0f8e50f
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-05-11 14:14:22 +0200

    mobile responsive done

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import Sass from "./main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  const circleSquare = document.querySelector(".circle-square-image");
+
+  circleSquare.addEventListener("click", function () {
+    event.preventDefault();
+    this.classList.remove("rotate");
+    void circleSquare.offsetWidth;
+    circleSquare.classList.add("rotate");
+  });
+
 });
