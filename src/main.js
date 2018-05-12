640fb7d0445de048723e3ac21cf7191a61c051d6
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-05-12 19:05:52 +0200

    changes in mobile and desktop RWD/fixed margin and font units

--- src/main.js
+++ src/main.js
@@ -20,14 +20,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const mobileNav = document.querySelector(".main-nav");
   const burgerButton = document.querySelector(".burger-button");
+  const burgerButtonImg = document.querySelector(".burger-button-img");
 
   burgerButton.addEventListener("click", function () {
     if (mobileNav.classList.contains("slide-down")) {
       mobileNav.classList.remove("slide-down");
       mobileNav.classList.add("slide-up");
+      setTimeout(() => {
+        burgerButtonImg.src = "./images/svg-files/hamburger_menu.svg";
+        burgerButtonImg.style.width = "9vw"
+      },200);
     } else {
       mobileNav.classList.remove("slide-up");
       mobileNav.classList.add("slide-down");
+      burgerButtonImg.src = "./images/svg-files/iks.svg";
+      burgerButtonImg.style.width = "7vw"
     }
   })
 
